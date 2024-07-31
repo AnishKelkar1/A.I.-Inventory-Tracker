a6d214033059868602cd50edd25ca1500d83652e
AnishKelkar1 <kelkaranish1@gmail.com>
2024-07-31 13:38:30 -0400

    connected to firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvYcBudTq3AbbbLias--KrAjweL3h-LEU",
+  authDomain: "ai-inventory-tracker.firebaseapp.com",
+  projectId: "ai-inventory-tracker",
+  storageBucket: "ai-inventory-tracker.appspot.com",
+  messagingSenderId: "583045350604",
+  appId: "1:583045350604:web:3f12389ea41c6432c6ee69"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
